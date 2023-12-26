023acbcd67c14c2734161a01e60ccf05bc070ed7
Remco Stoeten <remcostoeten@hotmail.com>
2023-12-26 14:33:21 +0100

    add css nesting support

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-nested'), // add this line
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }

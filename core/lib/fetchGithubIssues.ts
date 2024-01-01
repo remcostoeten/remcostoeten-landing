3a39dd010b2aa51d03ddf54384af6a39f8b3e2e8
Remco Stoeten <remcostoeten@hotmail.com>
2024-01-01 14:58:30 +0100

    add new table folder structure

--- core/lib/fetchGithubIssues.ts
+++ core/lib/fetchGithubIssues.ts
@@ -4,8 +4,16 @@ export const fetchGithubIssues = async () => {
   )
   const data = await response.json()
 
+  console.log(data)
+  // all data we want to display in table:
+  // state: "open" | "closed"
+  // created_)at: "2021-05-02T14:04:04Z"
+  // labels.name
+  // title
+
   return data.map((issue) => ({
     id: issue.id,
+    created_at: issue.created_at,
     code: `TASK-${issue.number}`,
     title: issue.title,
     status: issue.state === "open" ? "todo" : "done",

831bcf40b9e8c1b3ed028534730ae9d3df9c2d50
Remco Stoeten <remcostoeten@hotmail.com>
2024-01-23 18:06:34 +0100

    update styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import ShellLayout from "@/components/layout/MainLayoutShell"
 import SiteHeader from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 
-import "@/styles/globals.css"
+import "@/styles/globals.scss"
 import type { AppProps } from "next/app"
 import { TooltipProvider } from "@radix-ui/react-tooltip"
 import { Analytics } from "@vercel/analytics/react"
@@ -23,7 +23,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <AuthUserProvider>
       <ReduxProvider>
         <TooltipProvider>
-          <div
+          <divg
             className={cn(
               "body-gradient min-h-screen overflow-x-hidden bg-background font-sans antialiased",
               fontSora.variable
@@ -40,7 +40,7 @@ export default function App({ Component, pageProps }: AppProps) {
             </ThemeProvider>
             <SpeedInsights />
             <Analytics />
-          </div>
+          </divg>
         </TooltipProvider>
       </ReduxProvider>
     </AuthUserProvider>

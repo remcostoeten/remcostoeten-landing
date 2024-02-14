15fd455e70bda8975bf72e0ec67722c2e12ede78
Remco Stoeten <remcostoeten@hotmail.com>
2024-02-14 19:35:07 +0100

    add

---
+++ dashboard/components/ui/ui-imports.ts
@@ -0,0 +1,59 @@
+import { Accordion } from "./accordion";
+import { AlertDialog } from "./alert-dialog";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Checkbox } from "./checkbox";
+import { Dialog } from "./dialog";
+import { Popover } from "./popover";
+import { ScrollArea } from "./scroll-area";
+import { Select } from "./select";
+import { Slider } from "./slider";
+import { Switch } from "./switch";
+import { Tabs } from "./tabs";
+import { Button } from "./button";
+import { Form } from "./form";
+import { Label } from "./label";
+import { Alert } from "./alert";
+import { Card } from "./card";
+import { DataTable } from "./data-table";
+import { Input } from "./input";
+import { Modal } from "./modal";
+import { Separator } from "./separator";
+import { Skeleton } from "./skeleton";
+import { Textarea } from "./textarea";
+import { DropdownMenu } from "./dropdown-menu";
+import { RadioGroup } from "./radio-group";
+import { Calendar } from "./calendar";
+import { toast, Toaster } from "sonner";
+
+export {
+  Accordion,
+  AlertDialog,
+  Alert,
+  Avatar,
+  Button,
+  Calendar,
+  Card,
+  Checkbox,
+  DataTable,
+  Dialog,
+  DropdownMenu,
+  Form,
+  Input,
+  Label,
+  Modal,
+  Popover,
+  RadioGroup,
+  ScrollArea,
+  Select,
+  Separator,
+  Skeleton,
+  Slider,
+  Switch,
+  Tabs,
+  Textarea,
+  Toaster,
+  toast,
+  AvatarFallback,
+  AvatarImage,
+
+};

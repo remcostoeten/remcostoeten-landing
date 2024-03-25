787f598ecf5e0f5e814e9c2ce1f2e351659e7c28
Remco Stoeten <remcostoeten@hotmail.com>
2024-03-25 22:51:02 +0100

    remove blog

--- core/config/menu.ts
+++ core/config/menu.ts
@@ -3,7 +3,7 @@ import { Icons } from "@/components/icons"
 export const navigationMenuItems = [
   { label: "Home", icon: Icons.home, href: "/" },
   { label: "About", href: "about", icon: Icons.user },
-  { label: "Blog", icon: Icons.code, href: "blog" },
+  // { label: "Blog", icon: Icons.code, href: "blog" },
   { label: "Issues", icon: Icons.todo, href: "issues" },
   { label: "Kanban", icon: Icons.kanban, href: "kanban" },
   { label: "Guestbook", icon: Icons.PencilIcon, href: "guestbook" },

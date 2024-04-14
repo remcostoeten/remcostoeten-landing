582f1bf7410dd6ced76e1c61c5aa8c283e1f03af
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-14 14:39:14 +0200

    Fix filters

--- add-component.sh
+++
@@ -1,5 +0,0 @@
-# Run the npx shadcn-ui@latest add command with the provided component name
-npx shadcn-ui@latest add $1
-
-# Run the script to update the ui-imports.tsx file
-node update-imports.js

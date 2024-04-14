0261db9ca0aeb7151edbc095ad8079a0aac0232d
remcostoeten <remcostoeten@hotmail.com>
2024-04-14 03:04:19 +0200

    Init 1.0

--- core/database/ApolloWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import client from "@/core/database/ApolloClient"
-import { ApolloProvider } from "@apollo/client"
-
-export default function ApolloWrapper({ children }) {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>
-}
--- core/lib/countryToFlag.ts
+++
@@ -1,6 +0,0 @@
-import countryEmoji from "country-emoji"
-
-export const convertToEmoji = (country: string): string => {
-  const emoji = countryEmoji.flag(country) || "❓"
-  return emoji
-}

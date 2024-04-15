a976389e14f5a7fe67e847c7379bdb24a5a2b8f2
Remco Stoeten <remcostoeten@hotmail.com>
2024-04-15 15:22:16 +0200

    Update config to improve build times

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       "lh3.googleusercontent.com",
